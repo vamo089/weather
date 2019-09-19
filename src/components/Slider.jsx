6979c8742d13e3104e3a515b7e41fc46f76dd359
Yaroslav <yarumka@gmail.com>
2019-09-20 00:58:38 +0300

    weather,the Slider layout

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,14 +5,49 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 const Container = styled.div`
     display: flex;
-    width: 100%;
+    width: ${props => `${props.width }0%`};
+    padding: 50px;
+    margin: auto;
+    .rc-slider-mark{
+    	left: -33px;
+    }
 `;
 
+const railStyle = {
+	backgroundColor: 'rgba(0, 0, 0, 0.5)'
+};
+const trackStyle = {
+	backgroundColor: '#000'
+};
+const dotStyle = {
+	border: '2px solid #000'
+};
+const markStyle = {
+	color: '#000',
+	fontFamily: 'OpenSans,sans-serif',
+	fontSize: '23px',
+	strong:{
+		fontSize: '16px',
+		marginLeft: '2px'
+	}
+};
 
 const Day = () => {
+	let hoursCount = 8;
+	let data = {};
+	for (let i = 0; i < hoursCount; i++) {
+		let step = 100/hoursCount;
+
+
+		data[step * (i + 1)] = {
+			style: markStyle,
+			label: <span>3<strong style={markStyle.strong}>am</strong> </span>,
+		}
+	}
+
 	return (
-		<Container>
-			<Slider min={3} max={10}/>
+		<Container width={hoursCount}>
+			<Slider handleStyle={dotStyle} dotStyle={dotStyle} trackStyle={trackStyle} railStyle={railStyle} marks={data} step={null} />
 		</Container>
 	);
 };
